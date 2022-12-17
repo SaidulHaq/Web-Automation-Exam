cf3101ac4e41455ff01cf32a45ad5fbfa5567b80
sadi <sadi190896@gmail.com>
2022-12-17 09:05:52 +0600

    update file

--- Q4_JavaScriptAlerts.java
+++ Q4_JavaScriptAlerts.java
@@ -3,6 +3,7 @@ package ittbd.WebAutoXM1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import junit.framework.Assert;
 
@@ -20,33 +21,38 @@ public class Q4_JavaScriptAlerts extends Base {
 	public void Alerts() throws InterruptedException {
 		
 		WebElement result = driver.findElement(By.xpath("//p[@id='result']"));
-
-		// JS Alert------------------------
+		SoftAssert softAssert = new SoftAssert();
+		
+		
+		// JS Alert with Soft Assertion------------------------
 		driver.findElement(By.xpath("//button[contains(text(),'Click for JS Alert')]")).click();
 		String text = driver.switchTo().alert().getText();
 		System.out.println(text);
 		Thread.sleep(3000);
-		
 		driver.switchTo().alert().accept();
 		Thread.sleep(3000);
-		
-		String resultText = result.getText();
-		Assert.assertEquals(resultText, "You successfully clicked an alert");
+		String resultText1 = result.getText();
+		softAssert.assertEquals(resultText1, "You clicked");
 
 		
-		// JS Confirm------------------------
+		// JS Confirm with Hard Assertion------------------------
 		driver.findElement(By.xpath("//button[contains(text(),'Click for JS Confirm')]")).click();
 		Thread.sleep(3000);
 		driver.switchTo().alert().dismiss();
 		Thread.sleep(3000);
+		String resultText2 = result.getText();
+		Assert.assertEquals(resultText2, "You clicked: Cancel");
 
 		
-		// JS Alert---------------------------
+		// JS Prompt---------------------------
 		driver.findElement(By.xpath("//button[contains(text(),'Click for JS Prompt')]")).click();
-		driver.switchTo().alert().sendKeys("I AM SADI");
+		driver.switchTo().alert().sendKeys("Hi, I am Sadi from IT Training BD Batch 13");
 		Thread.sleep(3000);
 		driver.switchTo().alert().accept();
 		Thread.sleep(3000);
+		
+		
+		softAssert.assertAll();
 
 	}
 
